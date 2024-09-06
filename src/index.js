0bb0313c8b7ad41a1e26117100f621f3dc1845ce
thurzob <thurzob@kkszki.hu.com>
2024-09-06 10:24:12 +0200

    Initial commit

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import Home from './Home'
 import { BrowserRouter, Router } from 'react-router-dom';
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
@@ -12,6 +13,16 @@ root.render(
     </BrowserRouter>
   </React.StrictMode>
 );
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js').then((registration) => {
+      console.log('Service Worker regisztrálva:', registration);
+    }).catch((error) => {
+      console.log('Service Worker regisztrációs hiba:', error);
+    });
+  });
+}
 
 
 
